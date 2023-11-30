1ea65c7d77eada696c0c885b4cc8fea8be0f2872
GodOfOni <118010221+GodOfOni@users.noreply.github.com>
2023-11-30 16:18:43 +0100

    Reupload

---
+++ src/components/App.jsx
@@ -0,0 +1,28 @@
+// import { Home, Movies, MovieDetails } from '../pages';
+// import { Cast, Reviews, SharedLayout } from '../components';
+
+import { lazy } from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { SharedLayout } from '../components';
+
+const Cast = lazy(() => import('components/Cast/Cast'));
+const Reviews = lazy(() => import('components/Reviews/Reviews'));
+const Home = lazy(() => import('pages/Home/Home'));
+const MovieDetails = lazy(() => import('pages/MovieDetails/MovieDetails'));
+const Movies = lazy(() => import('pages/Movies/Movies'));
+
+export const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/movies/:movieId" element={<MovieDetails />}>
+          <Route path="/movies/:movieId/cast" element={<Cast />} />
+          <Route path="/movies/:movieId/reviews" element={<Reviews />} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Route>
+    </Routes>
+  );
+};
